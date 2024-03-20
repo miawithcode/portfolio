eac3d27eadb6cf73583ca47973c8af9d0795f3f2
miawithcode <cuttontail@gmail.com>
2024-03-20 18:15:23 +0800

    style: change fonts and style layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["var(--font-geist-sans)"],
+      mono: ["var(--font-geist-mono)"],
+    },
     extend: {},
   },
   plugins: [],
