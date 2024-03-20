cf5adfa8bd0f7b225aa691855f61c7633cb8197c
miawithcode <cuttontail@gmail.com>
2024-03-20 19:48:46 +0800

    chore: add skills data

--- src/lib/data.ts
+++
@@ -1,7 +0,0 @@
-export const navItems = [
-  { href: "/", label: "Home" },
-  { href: "#skills", label: "Tech Stack" },
-  { href: "#works", label: "Selected Work" },
-  { href: "#about", label: "About" },
-  { href: "#contact", label: "Contact" },
-];
